63edf01c4572f2c7715e197bf48c465763d7d77d
Joseph Pkhakadze <joseph.pkhakadze@trivadis.com>
2018-09-13 23:49:02 +0200

    add some docs

--- lambda-demo/src/main/java/com/trivadis/te2018/serverless/easiest/EasyLambda.java
+++ lambda-demo/src/main/java/com/trivadis/te2018/serverless/easiest/EasyLambda.java
@@ -1,7 +1,15 @@
 package com.trivadis.te2018.serverless.easiest;
 
+/**
+ * Very simple Java Function only with one parameter
+ *
+ */
 public class EasyLambda {
 	
+	/**
+	 * Entry point for a FaaS
+	 * @param s a string parameter
+	 */
 	public void handler(String s) {
         System.out.println("Hello " + s);
     }
